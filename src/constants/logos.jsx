0f02a9d919420d762fb45f0654482a85c0af9c35
ccuofano <carmine.cuofano@holcim.com>
2024-10-23 13:58:18 +0200

    fixie

--- src/constants/logos.jsx
+++ src/constants/logos.jsx
@@ -7,9 +7,11 @@ import pork from "../assets/alergenics/pork.svg";
 import alcool from "../assets/alergenics/alcool.svg";
 import pepper from "../assets/alergenics/pepper.svg";
 import tomato from "../assets/alergenics/tomato.svg";
-import chemistry from "../assets/alergenics/chemistry.svg";
 import chocolate from "../assets/alergenics/chocolate.svg";
 import ajo from "../assets/alergenics/garlic.svg";
+import olives from "../assets/alergenics/olives.svg";
+import champignon from "../assets/alergenics/champignon.svg";
+import chemistry from "../assets/alergenics/chemistry.svg";
 
 const typeLogo = new Map([
   ["gluten", gluten],
@@ -21,9 +23,11 @@ const typeLogo = new Map([
   ["alcool", alcool],
   ["pimienta-negra", pepper],
   ["tomate", tomato],
-  ["solfiti", chemistry],
   ["cacao", chocolate],
-  ["ajo", ajo]
+  ["ajo", ajo],
+  ["olive", olives],
+  ["champign", champignon],
+  ["solfiti", chemistry],
 ]);
 
 export default typeLogo;

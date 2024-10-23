4ceb19473543d4009d454e6215fb1dfb2e253dd2
AdrianaP00 <adrianapiccolo00@gmail.com>
2024-10-23 13:09:28 +0200

    add new svg

--- src/constants/logos.jsx
+++ src/constants/logos.jsx
@@ -5,6 +5,11 @@ import fish from "../assets/alergenics/fish.svg";
 import egg from "../assets/alergenics/egg.svg";
 import pork from "../assets/alergenics/pork.svg";
 import alcool from "../assets/alergenics/alcool.svg";
+import pepper from "../assets/alergenics/pepper.svg";
+import tomato from "../assets/alergenics/tomato.svg";
+import chemistry from "../assets/alergenics/chemistry.svg";
+import chocolate from "../assets/alergenics/chocolate.svg";
+import ajo from "../assets/alergenics/garlic.svg";
 
 const typeLogo = new Map([
   ["gluten", gluten],
@@ -13,7 +18,12 @@ const typeLogo = new Map([
   ["pescado", fish],
   ["huevo", egg],
   ["cerdo", pork],
-  ["alcool", alcool]
+  ["alcool", alcool],
+  ["pimienta-negra", pepper],
+  ["tomate", tomato],
+  ["solfiti", chemistry],
+  ["cacao", chocolate],
+  ["ajo", ajo]
 ]);
 
 export default typeLogo;
